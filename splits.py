1e37cb83e08baae9041f7f8e56161ca08b62e39b
Alex <jk1143298@gmail.com>
2024-01-28 15:45:18 +0100

    Program.main

--- splits.py
+++ splits.py
@@ -68,30 +68,6 @@ def split(s: str, delimiter: str =''):
         return [ *s ]
     return remake(s, delimiter)
 
-def asserts():
-    str1 = "  A B  C  "
-    str2 = "A B C"
-    str3 = ""
-    str4 = "ABBBCCDJKBCCDDDBCBCDR.TEABCCVA"
-
-    sys.stdout.write("asserts..." + '\n')
-
-    Assert.assert_v(remake('a', 'a'), 'a'.split('a'))
-    Assert.assert_v(split('s d1', 'd12'), 's d1'.split('d12'))
-    Assert.assert_v(split(str4, "BCC"), str4.split("BCC"))
-    Assert.assert_v(split(str1, " "), str1.split(" "))
-    Assert.assert_v(split(str2, " "), str2.split(" "))
-    Assert.assert_v(split(str3, "dd"), str3.split("dd"))
-    Assert.assert_v(split(str2, "D"), str2.split("D"))
-    Assert.assert_v(split(' ', ' '), ' '.split(' '))
-    Assert.assert_v(split('  ', ' '), '  '.split(' '))
-    Assert.assert_v(split('  ', '  '), '  '.split('  '))
-    Assert.assert_v(split('aaaaa', 'b'), 'aaaaa'.split('b'))
-    Assert.assert_v(split('1,2,,3,', ','), '1,2,,3,'.split(','))
-
-    for w_s in [ ' ', '\t', '\n', '\v', '\f', '\r']:
-        Assert.assert_v(split(w_s.join(['1', '2', '3', '4']), w_s), w_s.join(['1', '2', '3', '4']).split(w_s))
-
 def double_split(string, _split, *splits):
     a1 = split(string, _split) # string.split(_split); # the python_std version
     a2 = []
@@ -102,45 +78,75 @@ def double_split(string, _split, *splits):
         a2.clear()
     return a1
 
+class Program:
+
+    @classmethod
+    def asserts(cls):
+        str1 = "  A B  C  "
+        str2 = "A B C"
+        str3 = ""
+        str4 = "ABBBCCDJKBCCDDDBCBCDR.TEABCCVA"
+
+        sys.stdout.write("asserts..." + '\n')
+
+        Assert.assert_v(remake('a', 'a'), 'a'.split('a'))
+        Assert.assert_v(split('s d1', 'd12'), 's d1'.split('d12'))
+        Assert.assert_v(split(str4, "BCC"), str4.split("BCC"))
+        Assert.assert_v(split(str1, " "), str1.split(" "))
+        Assert.assert_v(split(str2, " "), str2.split(" "))
+        Assert.assert_v(split(str3, "dd"), str3.split("dd"))
+        Assert.assert_v(split(str2, "D"), str2.split("D"))
+        Assert.assert_v(split(' ', ' '), ' '.split(' '))
+        Assert.assert_v(split('  ', ' '), '  '.split(' '))
+        Assert.assert_v(split('  ', '  '), '  '.split('  '))
+        Assert.assert_v(split('aaaaa', 'b'), 'aaaaa'.split('b'))
+        Assert.assert_v(split('1,2,,3,', ','), '1,2,,3,'.split(','))
+
+        for w_s in [ ' ', '\t', '\n', '\v', '\f', '\r']:
+            Assert.assert_v(split(w_s.join(['1', '2', '3', '4']), w_s), w_s.join(['1', '2', '3', '4']).split(w_s))
+
+
+    @classmethod
+    def main(cls):
+        # print(double_split("aA--b-\ncSuper\nTrial\nSome\n\n\nWorking hours", "-", '\n', 'a', ' '));
+        cls.asserts()
+
+        print("#TIMING")
+
+        s = 'a  '*100_000
+
+        delimiter = ' '
+        start = time()
+        split(s, delimiter)
+        print(time() - start)
+
+        start = time()
+        s.split(delimiter)
+        print("native split: ", time() - start)
+
+        start = time()
+        cysplit(s, delimiter)
+        print(time() - start)
+
+
+        start = time()
+        split_pattern = "\W+"
+        re.split(split_pattern, s)
+        print(f"re.split(\"{split_pattern}\"): ", time() - start)
+
+
+
+        # s1 = bytes(s, 'utf-8')
+        start = time()
+        remake(s, delimiter)
+        # print('a  '.split('a') == list(remake(bytes('a  ', 'utf-8'), bytes('a', 'utf-8') )))
+        print("remake: ", time() - start)
+
+        start = time()
+        char.split(s, delimiter)
+        print(time() - start)
+
 
 
 if __name__ == '__main__' :
-    # print(double_split("aA--b-\ncSuper\nTrial\nSome\n\n\nWorking hours", "-", '\n', 'a', ' '));
-    asserts()
-
-    print("#TIMING")
-
-    s = 'a  '*100_000
-
-    delimiter = ' '
-    start = time()
-    split(s, delimiter)
-    print(time() - start)
-
-    start = time()
-    s.split(delimiter)
-    print("native split: ", time() - start)
-
-    start = time()
-    cysplit(s, delimiter)
-    print(time() - start)
-
-
-    start = time()
-    split_pattern = "\W+"
-    re.split(split_pattern, s)
-    print(f"re.split(\"{split_pattern}\"): ", time() - start)
-
-
-
-    # s1 = bytes(s, 'utf-8')
-    start = time()
-    remake(s, delimiter)
-    # print('a  '.split('a') == list(remake(bytes('a  ', 'utf-8'), bytes('a', 'utf-8') )))
-    print("remake: ", time() - start)
-
-    start = time()
-    char.split(s, delimiter)
-    print(time() - start)
-
-
+    Program.main()
