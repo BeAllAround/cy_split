c6bde9479fc4669b245d0a11177586345ff2cf52
Alex <jk1143298@gmail.com>
2024-01-28 15:58:59 +0100

    separator

--- draw_graphs.py
+++ draw_graphs.py
@@ -79,7 +79,7 @@ if __name__ == '__main__':
     plt.plot(sc, tc, label = str(remake))
     plt.plot(sc1, tc1, label = str(''.split))
 
-    plt.ylabel('Time Complexity (' + str(round(((avg-avg1) / avg1) * 100)) + '%' + ')' )
+    plt.ylabel('Time Complexity (' + str(round(((avg-avg1) / avg1) * 100)) + '%' + '), ' + 'separator: ' + '"' + deli + '"' )
     plt.xlabel('Space Complexity')
     plt.legend()
     plt.show()
