9fae104ec58088c2774c379161d8160acb5be7ef
Alex <jk1143298@gmail.com>
2024-01-31 16:27:47 +0100

    update setup info

--- setup.py
+++ setup.py
@@ -1,12 +1,26 @@
 from distutils.core import setup
+from setuptools import find_packages
 from distutils.core import Extension
 
 from Cython.Distutils import build_ext
 
 from Cython.Build import cythonize
 
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
 
 setup(
+        name="cy_split",
+        version="0.0.1",
+        description="Efficient split for Python",
+        long_description=readme,
+        author='Aleksandar Milenkovic',
+        author_email='milenkovicaleksandar470@gmail.com',
+        license=license,
+        packages=find_packages(exclude=('tests')),
         ext_modules=[
             Extension('cy',
                 sources=['./src/cy.pyx'],
