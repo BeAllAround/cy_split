5eb31d3a4ec05c3f12a7d35610b94f5344269087
Alex <jk1143298@gmail.com>
2024-02-02 17:36:30 +0100

    stdout grid

--- tests/quick.py
+++ tests/quick.py
@@ -6,45 +6,66 @@ from context import cy
 
 import py
 
+
+def add_row(s, t, w = 15):
+    t = str(t)
+    return ( '|' + s + (' ' * (w//2 - len(s))) + '|') + (t + (' ' * (w//2 - len(t))) + '|\n' + '|' + '-' * w + '|')
+    
+
+def print_grid(rows, h = 25, w = 45):
+    w+=2 # starting '|'
+    screen = []
+
+    screen.append('|' + '-' * w + '|')
+    for row in rows:
+        screen.append(add_row(*row, w))
+
+    print( '\n'.join(screen) )
+
 class Program:
 
     @classmethod
     def main(cls):
         # print(double_split("aA--b-\ncSuper\nTrial\nSome\n\n\nWorking hours", "-", '\n', 'a', ' '));
 
-        print("#TIMING")
+        rows = []
 
         s = 'a  '*100_000
 
         delimiter = ' '
         start = time()
         cy._split(s, delimiter)
-        print('cy._split', time() - start)
+        rows.append(['cy._split', time() - start])
 
         start = time()
         s.split(delimiter)
-        print("native split", time() - start)
+        rows.append(['<builtin> split', time() - start])
 
         start = time()
         py._split(s, delimiter)
-        print('py._split', time() - start)
+        rows.append(['py._split', time() - start])
 
         start = time()
         split_pattern = "\W+"
         re.split(split_pattern, s)
-        print(f"re.split(\"{split_pattern}\")", time() - start)
+        rows.append([f"re.split(\"{split_pattern}\")", time() - start])
 
 
 
         # s1 = bytes(s, 'utf-8')
         start = time()
         cy.split(s, delimiter)
+        rows.append(['cy.split', time() - start])
         # print('a  '.split('a') == list(remake(bytes('a  ', 'utf-8'), bytes('a', 'utf-8') )))
-        print("cy.split", time() - start)
+
 
         start = time()
         numpy.char.split(s, delimiter)
-        print("numpy.char.split", time() - start)
+        rows.append(['numpy.char.split', time() - start])
+
+        print("#TIMING")
+        print_grid(rows)
+
 
 if __name__ == '__main__':
     Program.main()
